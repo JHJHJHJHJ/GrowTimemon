20b05ae03d21827e2064e928c407f22543254d7c
DESKTOP-9OE40KJ\pc <gkdh1015@gmail.com>
2021-03-05 10:55:32 +0900

    버그 픽스 (새벽에 앱을 실행하면 당일 아침 5시에 초기화되지 않음)

--- Assets/Scripts/System/RealTimeHandler.cs
+++ Assets/Scripts/System/RealTimeHandler.cs
@@ -30,7 +30,14 @@ public class RealTimeHandler : MonoBehaviour
             dailyReward.ResetDailyReward();
             questManager.ResetQuestsHasCleared();
 
-            resetTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day + 1, resetHour, 0, 0);
+            if(DateTime.Now.Hour < resetHour)
+            {
+                resetTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, resetHour, 0, 0);
+            }
+            else
+            {
+                resetTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day + 1, resetHour, 0, 0);
+            }           
         }    
     }
 
